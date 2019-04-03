43e3afd91864faf8acace107f550c790596efdfc
Abubakar Lawal <abula3003@gmail.com>
2019-04-03 09:11:30 +0100

    Update invoice.blade.php

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -5,7 +5,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<title>Invoice</title>
 	{{-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> --}}
-	{{-- <style type="text/css">
+	<style type="text/css">
 	  .container {
 	    margin: 10px;
 	  }
@@ -35,7 +35,7 @@
 	    padding: 10px;
 	}
 
-</style> --}}
+</style>
 </head>
 <body>
 	<div class="container">
